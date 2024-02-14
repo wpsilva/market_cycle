eadf377e3f637a9b2b2430786e8a54a2619ce8cb
wagner <wagnerpdasilva@hotmail.com>
2024-02-13 21:49:19 -0300

    removendo o mes de janeiro da performance anual

--- analysing_market_cycle_jan.py
+++ analysing_market_cycle_jan.py
@@ -7,15 +7,20 @@ ibov = yf.download("^BVSP", start='2000-01-01', interval='1mo')
 ibov['ano'] = ibov.index.year
 # Calcular a variação percentual do IBOV em janeiro e no ano
 ibov["variacao_mes"] = ibov["Adj Close"].pct_change()
-ibov = ibov.dropna()
-ibov["variacao_ano"] = ibov.groupby(ibov.index.year)["variacao_mes"].transform("sum")
-#print(ibov["variacao_mes"].iloc[0:11].sum())
+#ibov = ibov.dropna()
+ibov_ano = ibov[ibov.index.month != 1]
+
+ibov["variacao_ano"] = ibov_ano.groupby(ibov_ano.index.year)["variacao_mes"].transform("sum")
 
 # Filtrar apenas os dados de janeiro
 ibov_janeiro = ibov[ibov.index.month == 1]
+#print((ibov[ibov.index.month != 1]["variacao_ano"].drop_duplicates()))
+ibov_janeiro["variacao_ano"] = (ibov[ibov.index.month != 1]["variacao_ano"].drop_duplicates()).values
+#print(ibov_janeiro["variacao_ano"])
 
 ibov_janeiro['resultado'] = np.where((ibov_janeiro['variacao_mes'] * ibov_janeiro['variacao_ano'] > 0), 1, 0)
 
 percentual_ocorrencias = ibov_janeiro['resultado'].value_counts(normalize=True) * 100
 
+print(ibov_janeiro)
 print(percentual_ocorrencias)
